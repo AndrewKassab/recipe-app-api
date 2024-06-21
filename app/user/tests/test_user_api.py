d7ef5099859ab874a577ad909b72d434529ce8e6
andrewkassab <andrewkassab@live.com>
2024-06-20 17:39:26 -0700

    fixed

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -132,10 +132,10 @@ class PrivateUserApiTests(TestCase):
         """Test retrieving profile for logged in user."""
         res = self.client.get(ME_URL)
 
-        self.assertEqual(res.status, status.HTTP_200_OK)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, {
             'name': self.user.name,
-            'email': self.user.password,
+            'email': self.user.email,
         })
 
     def test_post_me_not_allowed(self):
